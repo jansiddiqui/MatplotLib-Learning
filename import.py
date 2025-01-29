31730e7ede3776dc4ce0502f35141f339583799a
Jan <jan810693@gmail.com>
2025-01-29 12:03:27 +0530

    detailed

--- import.py
+++ import.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot as plt
 
-#Median Developer Salaries by Age
 dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
-# Median python developer salaries by age
-py_dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+plt.plot(dev_x, dev_y)
+plt.show()
+# # Median python developer salaries by age
+# py_dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
 
-#median js developer salaries by  age
-js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
+# #median js developer salaries by  age
+# js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
